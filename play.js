67bfc0f3e518038cca86fddfee78ee8f6a807eb9
Joey Chan <joeychan916@gmail.com>
2023-07-13 00:51:24 +0000

    Update play.js to use client module

--- play.js
+++ play.js
@@ -5,29 +5,10 @@ process.stdout.write('\x07');
 const { Game } = require('./src/Game');
 const { UserInterface } = require('./src/UserInterface');
 const { RemoteInterface } = require('./src/RemoteInterface');
-const net = require("net");
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost', // Enter the server IP address here
-    port: 3000, // Enter the server port number here
-  });
-
-  conn.setEncoding("utf8");
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log("Connecting ...");
 const connection = connect();
 
-connection.on("connect", () => {
-  console.log("Successfully connected to the server!");
-});
-
-connection.on("data", (data) => {
-  console.log("Server says:", data);
-});
-
 const game = new Game(new UserInterface(), new RemoteInterface(connection));
 game.start();
-
