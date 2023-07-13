8657099f4d33cd8bff958d1a20e7f687151c4622
Joey Chan <joeychan916@gmail.com>
2023-07-13 05:51:27 +0000

    Move connection setup and add name sending logic

--- client.js
+++ client.js
@@ -1,16 +1,19 @@
 const net = require("net");
 
+// establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 3000,
+    host: "localhost", // Enter the server IP address here
+    port: 3000, // Enter the server port number here
   });
 
+  // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // Event handler for successful connection
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-    conn.write("Name: JC");
+    conn.write("Name: JC"); // Send the name message to the server upon connection
   });
 
   return conn;
